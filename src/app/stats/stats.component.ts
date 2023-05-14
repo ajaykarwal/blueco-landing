51ae59d99d472a470902aad694227e16671c02eb
Ajay Karwal <ajaykarwal@gmail.com>
2023-05-14 09:44:35 +0100

    Add icons and update image

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -11,15 +11,18 @@ export class StatsComponent implements OnInit {
   ngOnInit(): void {}
   stats = [
     {
+      icon: 'icon-access.svg',
       title: 'Exclusive Access',
       content:
         'Unprecedented direct access to the Amazon Climate-Pledge Friendly internal team to fast-track listing.',
     },
     {
+      icon: 'icon-manage.svg',
       title: '$50m',
       content: 'Managed and optimized over $50 million in ad spend on Amazon.',
     },
     {
+      icon: 'icon-percent.svg',
       title: 'Top 1%',
       content:
         'Top 1% of Amazon agencies in the world with expertise in SEO, A+ content, Ad Ops and more.',
